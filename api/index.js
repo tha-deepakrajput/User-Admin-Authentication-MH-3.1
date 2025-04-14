e6d2eeb2d559a56ea494838bb9f546677db40e32
tha-deepakrajput <deepak23@navgurukul.org>
2025-04-14 13:37:29 +0530

    installed serverless-http

--- api/index.js
+++ api/index.js
@@ -1,25 +1,25 @@
-// const express = require('express');
-// const serverless = require('serverless-http');
-// const app = express();
-
-// app.use(express.json());
-// app.get('/', (req, res) => {
-//     res.send('Hello from Vercel Serverless!');
-// });
-
-// module.exports.handler = serverless(app);
-
-
-
-// api/index.js
 const express = require('express');
+const serverless = require('serverless-http');
 const app = express();
 
-// Your middlewares and routes here, like before
-
+app.use(express.json());
 app.get('/', (req, res) => {
-  res.render('index');  // or any view you need
+    res.send('Hello from Vercel Serverless!');
 });
 
-// Export a function that Vercel can run
-module.exports = app;
+module.exports.handler = serverless(app);
+
+
+
+// // api/index.js
+// const express = require('express');
+// const app = express();
+
+// // Your middlewares and routes here, like before
+
+// app.get('/', (req, res) => {
+//   res.render('index');  // or any view you need
+// });
+
+// // Export a function that Vercel can run
+// module.exports = app;
